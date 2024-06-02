d61f01885ced226268e644ecc60125ee780b606e
Manav Singh <manavjotsingh17@gmail.com>
2024-06-01 18:01:16 -0700

    updated make

--- make.sh
+++ make.sh
@@ -4,5 +4,13 @@ mkdir rain
 cp LICENSE.txt rain/LICENSE.txt
 cp README.md rain/README.md
 pyinstaller --onefile --distpath rain rain.py
-zip -r rain.zip rain
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    zip -r rain.zip rain
+elif [[ "$(uname)" =~ MINGW ]]; then
+    Compress-Archive -Path rain/* -DestinationPath rain.zip
+else
+    echo "Unknown operating system, unable to zip the folder."
+fi
+
 rm -rf rain
--- make_windows.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-mkdir rain
-cp LICENSE.txt rain/LICENSE.txt
-cp README.md rain/README.md
-pyinstaller --onefile --distpath rain rain.py
